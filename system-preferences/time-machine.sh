e44faeca572d94635026326f4955bee3dd9c06a3
Gethin Oakes <gethinoakes@gmail.com>
2018-06-03 23:57:55 +0100

    Update time-machine.sh

--- system-preferences/time-machine.sh
+++ system-preferences/time-machine.sh
@@ -9,5 +9,5 @@ echo "\033[34;7m applying system preferences: time machine \033[0m"
 # Prevent Time Machine from prompting to use new hard drives as backup volume
 defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
 
-# Disable local Time Machine backups
-hash tmutil &>/dev/null && sudo tmutil disablelocal
+# Disable local Time Machine backups - no longer works
+# hash tmutil &>/dev/null && sudo tmutil disablelocal
