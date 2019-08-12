e94e0ba600ca85089339339c6607cab95a89e321
Gethin Oakes <gethinoakes@gmail.com>
2019-08-12 10:25:58 +0100

    Update macos-setup.sh

--- new/macos-setup.sh
+++ new/macos-setup.sh
@@ -96,7 +96,7 @@ rm -rf fonts
 # development
 echo_warn "Installing dev stuff"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-nvm install node
+nvm install --lts
 brew cask install "ngrok"
 brew cask install "yarn"
 
