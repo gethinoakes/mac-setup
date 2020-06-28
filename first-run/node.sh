44b4aa8b48eecd7f4c6d1c4d45312f2d1db1a710
Gethin Oakes <gethinoakes@gmail.com>
2020-06-28 14:28:13 +0100

    use nvm straight away after install

--- first-run/node.sh
+++ first-run/node.sh
@@ -3,6 +3,11 @@
 ##########
 echo_warn "Installing node & global packages"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.2/install.sh | bash
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 nvm install node
 nvm use node
 
